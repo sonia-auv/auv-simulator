41730a6c1e96184172f724da355ea83962c314f8
BillyBigGun <fawill16@gmail.com>
2018-11-19 12:53:14 -0500

    Control rotation plusieurs objets

--- Assets/Script/ScnPrincipale/ControlEnvironnement.cs
+++ Assets/Script/ScnPrincipale/ControlEnvironnement.cs
@@ -7,7 +7,8 @@ public class ControlEnvironnement : MonoBehaviour {
     const int ANGLE_UN_TOUR = 360;
 
     int angleModeleInitial = 50;
-    Transform modele;
+    List<Transform> modeles;
+    GameObject[] obj;
 
     public int vitesseRotation = 1;
 
@@ -17,24 +18,35 @@ public class ControlEnvironnement : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        modele = GameObject.FindGameObjectsWithTag("BoundingObject")[0].GetComponentInChildren<Transform>();
-        modele.GetComponentInParent<Transform>().Rotate(50, 0, 0);
-	}
+        modeles = new List<Transform>();
+        obj = GameObject.FindGameObjectsWithTag("BoundingObject");
+
+        for(int i =0; i < obj.Length; ++i)
+        {
+            modeles.Add(obj[i].GetComponentInChildren<Transform>());
+            modeles[i].GetComponentInParent<Transform>().Rotate(50, 0, 0);
+
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
+
         TournerModele();
 	}
 
     void TournerModele()
     {
-        
-        modele.transform.Rotate(0, vitesseRotation, 0);
-        additionAngleLacet += vitesseRotation;
-        if(additionAngleLacet > ANGLE_UN_TOUR)
+        foreach(Transform t in modeles)
         {
-            modele.transform.Rotate(-vitesseRotation, 0, 0);
-            additionAngleLacet -= ANGLE_UN_TOUR;
+            t.transform.Rotate(0, vitesseRotation, 0);
+            additionAngleLacet += vitesseRotation;
+            if (additionAngleLacet > ANGLE_UN_TOUR)
+            {
+                t.transform.Rotate(-vitesseRotation, 0, 0);
+                additionAngleLacet -= ANGLE_UN_TOUR;
+            }
         }
+        
     }
 }
